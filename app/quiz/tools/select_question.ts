34e91a3335c0c4be2fe3869c584ec04cf7b026f0
chickenchi <herosofthestom12@gmail.com>
2026-01-17 21:56:48 +0900

    중복 표시 처리 안 한 부분 수정

--- app/quiz/tools/select_question.ts
+++ app/quiz/tools/select_question.ts
@@ -6,15 +6,26 @@ export const selectQuestion = (
   viewedQuiz: Set<number>,
   setViewedQuiz: (update: SetStateAction<Set<number>>) => void,
 ) => {
-  const unviewed = questionData.filter(
+  
+  let unviewed = questionData.filter(
     (q) => !viewedQuiz.has(q.questionNumber)
   );
-
+  
+  console.log(unviewed)
+  
   if (unviewed.length === 0) {
     setViewedQuiz(new Set());
+    unviewed = questionData;
   }
 
   const randomIndex = Math.floor(Math.random() * unviewed.length);
+  const selected = unviewed[randomIndex];
+
+  setViewedQuiz(prev => {
+    const next = new Set(prev);
+    next.add(selected.questionNumber);
+    return next;
+  });
   
   return unviewed[randomIndex];
 };
